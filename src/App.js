c6107267d6fb063f7d4d2a3de01b05c6688fa10e
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-12 11:12:37 +0600

    stacked area chart added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import LineChartPage from "./components/LineChart/LineChart";
+import StackedAreaChart from "./components/StackedAreaChart/StackedAreaChart";
 import VerticalLineChart from "./components/VerticalLineChart/VerticalLineChart";
 
-
+import './App.css'
 function App() {
   return (
-    <div >
-      <LineChartPage/>
-      <VerticalLineChart/>
+    <div className="Header">
+      <ul>
+        <li>
+          <LineChartPage />
+        </li>
+        <li>
+          <VerticalLineChart />
+        </li>
+        <li>
+          <StackedAreaChart />
+        </li>
+      </ul>
+
+
+
     </div>
   );
 }
